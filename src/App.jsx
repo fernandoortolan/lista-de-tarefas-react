0819ba04565d1f3808ba2e81721b7991f5950c25
Fernando Augusto Ortolan <fernandoortolan@outlook.com>
2026-09-08 13:15:03 -0300

    feat: adiciona uma iteração de lista

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,19 @@ function Header({ title }) {
 }
 
 function App() {
+  const tarefas = [
+    'Fazer exercício',
+    'Ler documentação',
+    'Lavar cafeteira',
+  ];
+
+  const itensDaLista = tarefas.map(tarefa => <li>{tarefa}</li>);
+
   return (
-    <Header title={"Lista de tarefas"} />
+    <>
+      <Header title={"Lista de tarefas"} />
+      <ul>{itensDaLista}</ul>
+    </>
   );
 }
 
